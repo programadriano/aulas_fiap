89c76347d07d50beab563b2edb1fb042cc5cef8c
Thiago S Adriano <tadriano.net@gmail.com>
2022-10-03 19:36:02 -0300

    segunda-aula

---
+++ FIAP.Tests/Models/ProdutoTests.cs
@@ -0,0 +1,56 @@
+﻿using FIAP.Models;
+using FluentAssertions;
+
+namespace FIAP.Tests.Models
+{
+    public class ProdutoTests
+    {
+        Produto _produto;
+        public ProdutoTests()
+        {
+            _produto = new Produto();
+            _produto.SalvarProduto("Cadeira");
+            _produto.SalvarProduto("Mesa");
+            _produto.SalvarProduto("Mouse");
+        }
+
+        [Fact]
+        public void Deve_salvar_Produto()
+        {
+            //Arrange
+          
+            //Act
+            var resultado = _produto.BuscarProdutoPorNome("Cadeira");
+
+            //Assert
+            resultado.Should().NotBeNull();
+
+        }
+
+        [Fact]
+        public void Deve_Buscar_Produto_Por_Nome_Retornar_Sucesso()
+        {
+            //Arrange          
+
+            //Act
+            var resultado = _produto.BuscarProdutoPorNome("Cadeira");
+
+            //Assert
+            resultado.Should().BeEquivalentTo("Cadeira");
+
+        }
+
+        [Fact]
+        public void Deve_Buscar_Produto_Por_Nome_Retornar_Valor_Default()
+        {
+            //Arrange          
+
+            //Act
+            var resultado = _produto.BuscarProdutoPorNome("Cadeira Giratória");
+
+            //Assert
+            resultado.Should().BeEquivalentTo("Produto não encontrado");
+
+        }
+    }
+}
---
+++ FIAP/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace FIAP.Models
+{
+    public class Produto
+    {
+        private static List<string> _produtoList = new List<string>();
+
+        public string SalvarProduto(string nomeDoProduto)
+        {
+            _produtoList.Add(nomeDoProduto);
+
+            return "Produto salvo com sucesso!";
+        }
+
+        public string BuscarProdutoPorNome(string nomeDoProduto)
+        {
+            return _produtoList.FirstOrDefault(x => x == nomeDoProduto, "Produto não encontrado");
+        }
+    }
+}
