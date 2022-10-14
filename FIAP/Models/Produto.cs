47a845159902d0b6f8ce116dd100e92d2817931e
Thiago S Adriano <tadriano.net@gmail.com>
2022-10-14 09:11:51 -0300

    adicionando teste de listar todos e deletar

--- FIAP/Models/Produto.cs
+++ FIAP/Models/Produto.cs
@@ -4,7 +4,7 @@ namespace FIAP.Models
 {
     public class Produto
     {
-        private static List<string> _produtoLista = new List<string>();
+        public static List<string> produtoLista = new List<string>();
         public string Nome { get; set; }
 
         public Produto(string nome)
@@ -16,13 +16,13 @@ namespace FIAP.Models
 
         public string SalvarProduto()
         {
-            _produtoLista.Add(Nome);
+            produtoLista.Add(Nome);
             return $"Produto {Nome} salvo com sucesso!";
         }
 
         public string BuscarProdutoPorNome(string nomeDoProduto)
         {
-            return _produtoLista.FirstOrDefault(x => x == nomeDoProduto, "Produto não encontrado");
+            return produtoLista.FirstOrDefault(x => x == nomeDoProduto, "Produto não encontrado");
         }
 
         public void ValidateEntity()
@@ -34,7 +34,7 @@ namespace FIAP.Models
 
         public bool Deletar(string nomeDoProduto)
         {
-            return _produtoLista.Remove(nomeDoProduto);
+            return produtoLista.Remove(nomeDoProduto);
         }
 
         public string Atualizar(string nomeAntigo, string nomeNovo)
@@ -54,7 +54,11 @@ namespace FIAP.Models
 
         public List<string> ListarTodos()
         {
-            return _produtoLista.ToList();
+            //if (!produtoLista.Any())
+            //    produtoLista = new List<string>();
+
+            return produtoLista.ToList();
         }
+        
     }
 }
